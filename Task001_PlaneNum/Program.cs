3ce96d6ab77a428b84f63a008689c819b6a5a596
Pavel <m83819@gmail.com>
2022-08-14 11:17:53 +0700

    Task001_PlaneNum

--- Task001_PlaneNum/Program.cs
+++ Task001_PlaneNum/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+1. Напишите программу, которая принимает на вход 
+координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и 
+выдаёт номер четверти плоскости, в которой находится 
+эта точка.
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+
+int PlaneNum(int x, int y)
+{
+    if(x>0&&y>0) 
+    {
+        Console.WriteLine("Your point belongs in 1st quadrant!");
+        return 1;
+    }
+
+    if(x<0&&y>0)
+    {
+        Console.WriteLine("Your point belongs in 2nd quadrant!");
+        return 2;
+    } 
+    
+    if(x<0&&y<0) 
+    {
+        Console.WriteLine("Your point belongs in 3rd quadrant!");
+        return 3;
+    }
+    
+    if(x>0&&y<0) 
+    {
+        Console.WriteLine("Your point belongs in 4th quadrant!");
+        return 4;
+    }
+
+    if (x==0||y==0) Console.WriteLine("Your point lays on the plane border");
+    return 0;
+
+}
+
+
+int X=0;
+int Y=0;
+X=Prompt("enter x of your point:");
+Y=Prompt("enter y of your point:");
+PlaneNum(X,Y);
